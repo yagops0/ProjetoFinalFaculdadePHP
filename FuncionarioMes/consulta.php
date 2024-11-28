6da1f2e37dbd7caf8e8a389021685debaf160862
yagops0 <yagosalvador34@gmail.com>
2024-11-28 00:59:44 -0300

    Continuando os scripts do módulo de consulta

--- FuncionarioMes/consulta.php
+++ FuncionarioMes/consulta.php
@@ -4,13 +4,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Consulta</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="css/estilo.css">
 </head>
 <body>
     <div class="container">
         <br>
         <button> <a href="javascript: history.back()">Voltar </a> </button>
-        <h2>CONSULTA</h2>
-        <br>
+        <div class="row">
+            <div class="col-lg-6 mx-auto">
+                <h2 class="page-section-heading text-center text-uppercase text-secondary">CONSULTA</h2>
+                <br>
+                <form method="post" action="consultanome.php">
+                    <div class="form-group">
+                        <div class="control-group">
+                            
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
 
         
     </div>
